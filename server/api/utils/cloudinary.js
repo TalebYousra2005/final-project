e710abcb09f9958a094b6663f24e1cd2e4326661
Yousra <yousrataleb55@gmail.com>
2022-11-15 10:05:29 +0100

    configuring cloudinary with account information

---
+++ server/api/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dy4bwfzua",
+  api_key: "466586395751372",
+  api_secret: "o2QT9j9fS_yksID7RdVxZn58EbA",
+});
+
+module.exports = cloudinary;
